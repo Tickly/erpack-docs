ad0137d9a549a6675b4463cd0ea9bc0744e45544
taoguo <807081817@qq.com>
2021-11-04 11:07:27 +0800

    添加表单模型示例

---
+++ docs/.vuepress/components/form/with-model.vue
@@ -0,0 +1,45 @@
+<script>
+import { Model, StringProperty, NumberProperty } from 'erpack'
+
+class User extends Model {
+  properties () {
+    return {
+      name: new StringProperty({ label: '姓名', required: true }),
+      age: new NumberProperty({
+        label: '年龄',
+        required: true,
+        min: 1,
+      }),
+    }
+  }
+}
+
+export default {
+  data () {
+    return {
+      user: new User(),
+    }
+  },
+  methods: {
+    handleClick () {
+      this.$refs.form.validate()
+    }
+  },
+  render () {
+    const { user } = this
+    return (
+      <erp-form ref="form" form={user} layout="inline">
+        <erp-form-item prop="name">
+          <a-input vModel={user.name} />
+        </erp-form-item>
+        <erp-form-item prop="age">
+          <a-input-number vModel={user.age} />
+        </erp-form-item>
+        <erp-form-item>
+          <a-button type="primary" onClick={this.handleClick}>验证一下</a-button>
+        </erp-form-item>
+      </erp-form>
+    )
+  }
+}
+</script>
--- docs/components/form/README.md
+++ docs/components/form/README.md
@@ -4,3 +4,9 @@
 
 搭配布局组件`Grid`来使用。
 <form-basic-usage />
+
+<<< @/docs/.vuepress/components/form/basic-usage.vue
+
+## 配合模型
+<form-with-model />
+<<< @/docs/.vuepress/components/form/with-model.vue
