67e51044c37ae00de9ea9235257ff32db70757a2
Tickly <807081817@qq.com>
2021-11-13 00:28:38 +0800

    完善操作按钮文档

--- docs/.vuepress/components/table/action-column-demo.vue
+++ docs/.vuepress/components/table/action-column-demo.vue
@@ -3,6 +3,7 @@ export default {
   data() {
     return {
       limit: 3,
+      showDivider: true,
     };
   },
   computed: {
@@ -12,14 +13,22 @@ export default {
         {
           type: "action",
           limit: this.limit,
+          showDivider: this.showDivider,
           buttons: [
             {
               text: "查看",
               visible({ id }) {
+                console.log(id);
                 return id > 1;
               },
             },
-            { text: "编辑" },
+            {
+              text: "编辑",
+              visible: (row, index) => {
+                console.log(row);
+                return index === 1;
+              },
+            },
             {
               text: "删除",
               confirm: "确定删除？",
@@ -30,8 +39,8 @@ export default {
             {
               text: "启用",
               confirm: "确定启用？",
-              click() {
-                console.log("已启用");
+              click(row, index) {
+                console.log("已启用", row, index);
               },
             },
             { text: "禁用" },
@@ -48,6 +57,9 @@ export default {
         <erp-form layout="inline">
           <erp-form-item label="按钮数量">
             <a-input-number vModel={this.limit} />
+          </erp-form-item>
+          <erp-form-item label="显示分割线">
+            <a-checkbox vModel={this.showDivider} />
           </erp-form-item>
         </erp-form>
         <erp-table row-key="id" columns={this.columns} data-source={source} />
